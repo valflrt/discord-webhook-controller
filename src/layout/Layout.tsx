8b9ffa66d7267ab0909a7de500c09b7058b0c036
val <vfleurit@gmail.com>
2022-04-07 15:13:58 +0000

    remove react-router-dom and routing related components

--- src/layout/Layout.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC } from "react";
-
-import LayoutStyles from "./Layout.styles";
-
-const Layout: FC = ({ children }) => (
-  <LayoutStyles.Layout>{children}</LayoutStyles.Layout>
-);
-
-export default Layout;
