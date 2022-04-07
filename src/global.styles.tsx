f199ec88227f56de3d03fc8480c84a93a726b808
val <vfleurit@gmail.com>
2022-04-07 15:42:43 +0000

    edit global styles

--- src/global.styles.tsx
+++ src/global.styles.tsx
@@ -1,24 +1,60 @@
 import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
-  html, body {
+  html,
+  body {
     width: 100%;
     height: 100%;
+
     padding: 0;
     margin: 0;
   }
 
   * {
+    color: var(--tone-1);
     box-sizing: border-box;
+    outline: none;
   }
 
-  #root {
-    width: 100%;
-    height: 100%;
+  ::selection {
+    background-color: var(--primary-1);
+  }
 
-    overflow:  hidden auto;
+  *:focus {
+    outline: 2px solid var(--primary);
+  }
 
-    color: #f1f1f1;
-    background-color: #16161a;
+  :root {
+    --primary: hsl(267, 100%, 60%);
+    --primary-1: hsl(267, 50%, 60%);
+    --error: rgb(224, 81, 81);
+    --tone-1: hsl(267, 100%, 92%);
+    --tone-2: hsl(267, 40%, 75%);
+    --tone-3: hsl(267, 5%, 15%);
+    --tone-4: hsl(267, 5%, 10%);
+  }
+
+  body {
+    padding: 20px;
+    font-size: 14px;
+
+    font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+      "Lucida Sans", Arial, sans-serif;
+    background-color: var(--tone-4);
+  }
+
+  input,
+  button {
+    background-color: var(--tone-3);
+    border: 1px solid var(--tone-1);
+  }
+
+  button {
+    padding: 4px;
+  }
+
+  input {
+    width: 300px;
+    padding: 4px 2px;
   }
 `;
