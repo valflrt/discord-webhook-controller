66334c296155a11876ce07e65a73da159466bcc7
valflrt <vfleurit@gmail.com>
2022-04-09 19:37:59 +0200

    remove "scripts" directory and "deploy" script in package.json now uses gh-pages cli

--- scripts/deploy.js
+++
@@ -1,9 +0,0 @@
-const pages = require("gh-pages");
-
-pages.publish(
-  `${__dirname}/../build`,
-  {
-    branch: "pages",
-  },
-  (err) => console.log(err ?? "Successfully published to github pages")
-);
